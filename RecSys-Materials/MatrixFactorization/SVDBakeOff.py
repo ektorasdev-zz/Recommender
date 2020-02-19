e7e60ba595d27a5c4f0738f2cf8b980d4fbd5266
Ektoras <hector.drandakis@gmail.com>
2020-02-19 17:18:45 +0200

    Refactoring/Adding more detailed comments in main.py

--- RecSys-Materials/MatrixFactorization/SVDBakeOff.py
+++ RecSys-Materials/MatrixFactorization/SVDBakeOff.py
@@ -9,6 +9,7 @@ from MovieLens import MovieLens
 from surprise import SVD, SVDpp
 from surprise import NormalPredictor
 from Evaluator import Evaluator
+from surprise.model_selection import GridSearchCV
 
 import random
 import numpy as np
@@ -26,6 +27,13 @@ random.seed(0)
 
 # Load up common data set for the recommender algorithms
 (ml, evaluationData, rankings) = LoadMovieLensData()
+
+print("Searching for best parameters...")
+param_grid = {'n_epochs': [20, 30], 'lr_all': [0.005, 0.010],
+              'n_factors': [50, 100]}
+gs = GridSearchCV(SVD, param_grid, measures=['rmse', 'mae'], cv=3)
+
+gs.fit(evaluationData)
 
 # Construct an Evaluator to, you know, evaluate them
 evaluator = Evaluator(evaluationData, rankings)
